a70c1ca22ad8a536c6c09e397c0795085863527d
vqanh77 <vqanh77@gmail.com>
2023-12-16 23:13:51 +0700

    update: syntax

--- app/place/detail/[id]/page.jsx
+++ app/place/detail/[id]/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
 import React from "react";
-import React from 'react';
 import Navbar from '../../Navbar';
 import Detail from './Detail'
 import EditButton from './EditButton';
